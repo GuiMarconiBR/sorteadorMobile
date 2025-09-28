cb139e9464c218e000cc83cfb38fb72f034971e7
Guilherme Marconi <guilherme.marconi12@hotmail.com>
2025-09-27 21:07:49 -0300

    Apresentacao
    apresentacao

--- sorteadorMobile/src/screens/SorteioScreen.js
+++ sorteadorMobile/src/screens/SorteioScreen.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Dimensions} from "react-native";
 
-export default function SorteioScreen() {
-  return (
-    <View>
-      <Text>Tela de Sorteio</Text>
-    </View>
-  );
-}
+const screenWidth = Dimensions.get('window').width;
+
+export default function HomeScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>🎲 EM DESENVOLVIMENTO 🎲</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingVertical: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize:26,
+    },
+});
